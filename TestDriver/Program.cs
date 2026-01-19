458253f1d4f86438133bf43af6b59cd574940e34
MonsterLlama <MonsterLlama@gmail.com>
2026-01-18 18:48:48 -0800

    Implementing Solutoins and Test for Problem 746

---
+++ TestDriver/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MonsterLlama.Leetcode.TestDriver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var problem746 = new Problem746();
+
+            var result746c  = problem746.Test3();
+            var result746c1 = problem746.Test3();
+            var result746a  = problem746.Test1();
+            var result746a1 = problem746.Test1();
+            var result746b  = problem746.Test2();
+            var result746b1 = problem746.Test2();
+        }
+    }
+}
